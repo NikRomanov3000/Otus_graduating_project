193cf8761e3df3536f7f4bbc84d0c42bba67faa0
Nikita <nicromanov3000@gmail.com>
2020-05-25 00:59:54 +0300

    Fix ComponentScan

--- src/main/java/ru/romanov/graduation/project/Application.java
+++ src/main/java/ru/romanov/graduation/project/Application.java
@@ -3,15 +3,9 @@ package ru.romanov.graduation.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({
-        "ru.romanov.graduation.project.repository",
-        "ru.romanov.graduation.project.service",
-        "ru.romanov.graduation.project.controller"
-})
 @EntityScan(basePackages="ru.romanov.graduation.project.model")
 @EnableJpaRepositories(basePackages="ru.romanov.graduation.project.repository")
 public class Application {
