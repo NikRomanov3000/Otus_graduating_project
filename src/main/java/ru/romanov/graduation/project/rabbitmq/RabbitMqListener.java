8a5a0a8d166e701c3e3658b954490620f7910693
Nikita <nicromanov3000@gmail.com>
2020-06-23 19:15:35 +0300

    add RabbitMQ config and listener

---
+++ src/main/java/ru/romanov/graduation/project/rabbitmq/RabbitMqListener.java
@@ -0,0 +1,34 @@
+package ru.romanov.graduation.project.rabbitmq;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import ru.romanov.graduation.project.service.ReceiptService;
+import ru.romanov.otus.model.PaymentInfo;
+
+@EnableRabbit
+@Component
+public class RabbitMqListener {
+
+    private final ReceiptService receiptService;
+
+    public RabbitMqListener(ReceiptService receiptService) {
+        this.receiptService = receiptService;
+    }
+
+    @RabbitListener(queues = "paymentQueue")
+    public void processPaymentQueue(String message) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        PaymentInfo paymentInfoFromRabbit;
+
+        try {
+            paymentInfoFromRabbit = objectMapper.readValue(message, PaymentInfo.class );
+        } catch (JsonProcessingException ex){
+            throw new RuntimeException("Message from RabbitMQ does not match PaymentInfo format");
+        }
+
+        receiptService.updateReceipt(paymentInfoFromRabbit);
+    }
+}
